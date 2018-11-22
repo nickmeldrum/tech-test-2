d459b8e259e8ffeed7bca2046fb9426da6f70830
Nick Meldrum <nick@nickmeldrum.com>
2018-11-22 19:46:52 +0000

    task 1: problem definition

--- src/module1.js
+++ src/module1.js
@@ -1,1 +1,14 @@
 module.exports = () => 'hello worlds'
+
+// input is a string phone number
+// input contains digits, spaces and/or dashes
+// input contains at least 2 digits
+//
+// ignore spaces and dashes
+//
+// reformat string grouped in blocks of 3 separated by dashes
+// the final block OR last 2 blocks can be of length 2!!!
+//
+// N (length of string) is an integer 2..100
+// correctness only not performance
+//
--- src/module1.test.js
+++ src/module1.test.js
@@ -1,7 +1,13 @@
 const module1 = require('src/module1')
 
 describe('module1', () => {
-  test('outputs hello worlds', () => {
-    expect(module1()).toEqual('hello worlds')
+  ;[
+    { input: '00-44  48 5555 8361', output: '004-448-555-583-61' },
+    { input: '0 - 22 1985--324', output: '022-198-53-24' },
+    { input: '555372654', output: '555-372-654' },
+  ].forEach(testData => {
+    test('outputs hello worlds', () => {
+      expect(module1(testData.input)).toEqual(testData.output)
+    })
   })
 })
